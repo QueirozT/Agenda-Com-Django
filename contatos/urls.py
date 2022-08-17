b43836d8713230288639e36adfe7fe4fbeb86be8
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-08-17 07:51:27 -0300

    Criando um endpoint para a busca

--- contatos/urls.py
+++ contatos/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),  # Chamando a view index do contatos
+    path('busca/', views.busca, name='busca'),
     path('<int:contato_id>', views.detalhes, name='detalhes'),
 ]
