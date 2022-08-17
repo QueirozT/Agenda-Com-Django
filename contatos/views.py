9cde8889299bcad21318488d92b23c34a799bdf3
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-08-16 23:16:48 -0300

    Corrigindo o código de erro retornado

--- contatos/views.py
+++ contatos/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Contato
 
 def index(request):
     contatos = Contato.objects.all()
     return render(request, 'contatos/index.html', {'contatos': contatos})
+
+
+def detalhes(request, contato_id):
+    contato = get_object_or_404(Contato, id=contato_id)
+    return render(request, 'contatos/detalhes.html', {'contato': contato})
