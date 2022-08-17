b358c286b46817c0eb68d69bfea27674c1788ecb
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-08-17 06:40:29 -0300

    Importando o paginator do django e configurando para o index

--- contatos/views.py
+++ contatos/views.py
@@ -1,9 +1,16 @@
-from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
+from django.shortcuts import get_object_or_404, render
+
 from .models import Contato
 
 
 def index(request):
     contatos = Contato.objects.all()
+    paginator = Paginator(contatos, 10)
+
+    page = request.GET.get('p')
+    contatos = paginator.get_page(page)
+
     return render(request, 'contatos/index.html', {'contatos': contatos})
 
 
