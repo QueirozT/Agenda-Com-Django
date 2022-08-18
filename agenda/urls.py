d7fd35703eeb3314d758c01d0e031a9a7f264b07
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-08-18 11:42:58 -0300

    Registrando o endpoint das contas

--- agenda/urls.py
+++ agenda/urls.py
@@ -20,5 +20,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('contatos.urls')), # incluindo a rota urls do contatos
+    path('accounts/', include('accounts.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
